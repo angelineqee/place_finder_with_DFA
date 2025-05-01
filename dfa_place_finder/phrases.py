7a5a85ae87a1f3d10ec60fe07d4136d631649406
Dean Lau <ahdeanlau@gmail.com>
2025-05-01 20:29:07 +0800

    feat: create predefined accepted_phrases

---
+++ dfa_place_finder/phrases.py
@@ -0,0 +1,14 @@
+# dfa_place_finder/phrases.py
+# Single source of truth for the assignment's accepted items
+ACCEPTED_PHRASES: list[str] = [
+    "Malaysia", "Thailand", "Singapore", "Vietnam", "Indonesia", "Brunei",
+    "Philippines", "Malaya", "North Borneo", "British Empire",
+    "Peninsular Malaysia", "East Malaysia", "Malaysian Borneo", "Sarawak",
+    "Tanjung Piai", "Straits Settlements", "Malayan Union",
+    "Federation of Malaya", "Kuala Lumpur", "Putrajaya",
+    "Southeast Asia", "continental Eurasia", "tropics", "Malay kingdoms",
+    "British protectorates", "Association of Southeast Asian Nations",
+    "East Asia Summit", "Organisation of Islamic Cooperation",
+    "Asia-Pacific Economic Cooperation", "Commonwealth of Nations",
+    "Non-Aligned Movement",
+]
